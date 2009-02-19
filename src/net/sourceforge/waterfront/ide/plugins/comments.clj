44e9fd76ab3e5cdcb5a081aac3baff5443751b83
imaman <imaman@aeabe084-532a-4f5f-9356-c178316a5679>
2009-02-19 12:33:57 +0000

    Buf fix: comments.clj threw exception when commenting in the very last position
    git-svn-id: https://waterfront.svn.sourceforge.net/svnroot/waterfront/trunk/waterfront@114 aeabe084-532a-4f5f-9356-c178316a5679

--- src/net/sourceforge/waterfront/ide/plugins/comments.clj
+++ src/net/sourceforge/waterfront/ide/plugins/comments.clj
@@ -10,38 +10,43 @@
 
 (defn- toggle-comment [app start end]
   (let [src (.getText (app :area))
-        from (get-line-start src (min start end))
-        to  (max (inc from) (if (zero? (column-of src (max start end)))
+        from (get-line-start src (min start end))]
+
+    (if (>= from (count src))
+      app
+      (let [to  (min (count src) (max (inc from) (if (zero? (column-of src (max start end)))
                               (max start end) 
-                              (get-line-end src (max start end))))
+                              (get-line-end src (max start end)) )))
         turn-comment-off (= \; (.charAt src from))
         sb (new StringBuilder)]
-    (loop [i from 
-            line-start true]
-      (when (< i to)
-        (let [c (.charAt src i)]
-          (cond
-            (and line-start (not turn-comment-off))
-            (do (.append sb ";") (.append sb c))
-
-            (and line-start turn-comment-off (= c \;))
-            nil
-
-            (and line-start turn-comment-off (not= c \;))
-            (.append sb c)
-
-            :else
-            (.append sb c) ))
-
-        (recur (inc i) (= \newline (.charAt src i))) ))
-    (.select (app :area) from to)
-    (.replaceSelection (app :area) (str sb)) ))
+      (loop [i from 
+              line-start true]
+        (when (< i to)
+          (let [c (.charAt src i)]
+            (cond
+              (and line-start (not turn-comment-off))
+              (do (.append sb ";") (.append sb c))
+  
+              (and line-start turn-comment-off (= c \;))
+              nil
+  
+              (and line-start turn-comment-off (not= c \;))
+              (.append sb c)
+  
+              :else
+              (.append sb c) ))
+  
+          (recur (inc i) (= \newline (.charAt src i))) ))    
+      ((app :dispatch) (create-undo-transaction (fn [app]
+        (.select (app :area) from to) 
+        (.replaceSelection (app :area) (str sb)) )))
+      app ))))
 
 
 (fn [app] 
   (add-to-menu (load-plugin app "undo.clj") "Source" 
     { :name "Toggle Comment" :mnemonic KeyEvent/VK_T :key KeyEvent/VK_SEMICOLON :on-context-menu true
-      :action (create-undo-transaction (fn [app] (toggle-comment app (.getSelectionStart (app :area)) (.getSelectionEnd (app :area))))) }))
+      :action (fn [app] (toggle-comment app (.getSelectionStart (app :area)) (.getSelectionEnd (app :area)))) }))
 
 
 
