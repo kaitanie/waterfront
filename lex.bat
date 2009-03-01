ebdf5bb616c06c1fd5240c98b955fdf217d9617e
imaman <imaman@aeabe084-532a-4f5f-9356-c178316a5679>
2009-03-01 11:45:48 +0000

    Project restructuring completed: waterfront contains both .java and .clj sources
    git-svn-id: https://waterfront.svn.sourceforge.net/svnroot/waterfront/trunk/waterfront@154 aeabe084-532a-4f5f-9356-c178316a5679

--- lex.bat
+++
@@ -1,1 +0,0 @@
-@java -cp src;..\lab\bin\;c:\tools\clojure\clojure.jar clojure.lang.Repl src\net\sourceforge\waterfront\ide\services\lexer.clj 
--- wf.bat
+++ wf.bat
@@ -1,5 +1,5 @@
 @echo off
-java -Dnet.sourceforge.waterfront.plugins=%~dp0clj/net/sourceforge/waterfront/ide/plugins/ -cp clj;..\lab\bin\;c:\tools\clojure\clojure.jar clojure.main clj/net/sourceforge/waterfront/ide/main.clj %*
+java -Dnet.sourceforge.waterfront.plugins=%~dp0clj/net/sourceforge/waterfront/ide/plugins/ -cp clj;bin\;c:\tools\clojure\clojure.jar clojure.main clj/net/sourceforge/waterfront/ide/main.clj %*
 
 
 
